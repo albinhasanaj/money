cd8a63debc3d66ac79da4c505811b51ce93efdb9
Oliversoder06 <lexrudolf06@gmail.com>
2024-07-31 14:03:29 +0200

    Nu finns fin publsih sak

--- app/publish/page.tsx
+++ app/publish/page.tsx
@@ -7,19 +7,31 @@ const Publish = ({ toggleView, title }: { toggleView: boolean, title: string }) 
   const [sources, setSources] = React.useState([""])
   const [view, setView] = React.useState(toggleView)
   const [addTitle, setAddTitle] = React.useState(title)
+  const [addTextArea, setAddTextArea] = React.useState([""])
+  const [showTextArea, setShowTextArea] = React.useState(false)
   const toggleHidden = (e: any) => {
     console.log(e.target.innerText)
     setView(!view)
     if (e.target.innerText == "Project") {
       setAddTitle("Project")
+      setShowTextArea(true)
     } else if (e.target.innerText == "Learn") {
       setAddTitle("Learn")
+      setShowTextArea(false)
     } else if (e.target.innerText == "Review") {
       setAddTitle("Review")
+      setShowTextArea(false)
     }
   }
 
 
+  const handleAddTextArea = () => {
+    setAddTextArea([...addTextArea, ""])
+  }
+
+  if (sources.length > 1 && sources[sources.length - 1] === "") {
+    sources.pop()
+  }
 
   return (
     <div>
@@ -38,15 +50,37 @@ const Publish = ({ toggleView, title }: { toggleView: boolean, title: string }) 
       </section>
 
       <section className={view ? 'w-full flex flex-col' : 'hidden'}>
-        <div className='w-[756px] h-[813px] rounded-[15px] bg-[#E2E2E2] flex flex-col items-center py-5'>
+        <div className='w-[756px] h-auto rounded-[15px] bg-[#E2E2E2] flex flex-col items-center py-5'>
+          <button className='self-start text-3xl' onClick={toggleHidden}>⬅️ bakc</button>
           <h1 className='text-black text-2xl'>{addTitle}</h1>
-          <div>
-            <Input placeholder='Title' />
-            <Input placeholder='description about project u type here' />
-            {sources.map((source: any, index: any) => {
-              return <Input key={index} placeholder="Source" setSources={setSources} sources={sources} />
-            }
+          <div className='flex gap-10 mt-8 flex-col justify-between h-full'>
+            <div className='flex gap-4'>
+              <div className='flex flex-col gap-4'>
+                <Input placeholder='Title*' />
+                <Input placeholder='Description*' />
+              </div>
+              <div className='flex flex-col gap-4'>
+                {sources.map((source: any, index: any) => {
+                  return <Input key={index} placeholder="Source" setSources={setSources} sources={sources} />
+                }
+                )}
+              </div>
+            </div>
+            {showTextArea && (
+              <div className='flex flex-col items-center gap-8'>
+                {addTextArea.map((textArea: any, index: any) => {
+                  return <textarea
+                    name=""
+                    id=""
+                    placeholder='test'
+                    className='h-[200px] block px-5 py-3 w-[600px] border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none '></textarea>
+                })}
+                <button onClick={handleAddTextArea}>Add more Help</button>
+              </div>
             )}
+            <div className='w-full flex justify-center'>
+              <button className='w-[300px] h-[45px] border-black border-[2px] bg-[#cac9c9] rounded-md'>Publish</button>
+            </div>
           </div>
         </div>
       </section>
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 const Input = ({ placeholder, setSources, sources }: { placeholder: string, setSources?: any, sources?: any }) => {
 
+    const autoFocus = React.useRef<HTMLInputElement>(null)
     const [input, setInput] = React.useState('')
+
     const handleInput = (e: any) => {
         setInput(e.target.value)
         if (placeholder === "Source" && e.key === 'Enter') {
@@ -11,8 +14,22 @@ const Input = ({ placeholder, setSources, sources }: { placeholder: string, setS
         }
     }
 
+    useEffect(() => {
+        if (autoFocus.current) {
+            autoFocus.current.focus()
+        }
+    },
+        [sources, placeholder])
+
+
+
     return (
-        <input onKeyDown={handleInput} type="text" placeholder={placeholder} className='py-3 px-4 block w-[400px] border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none ' />
+        <input
+            ref={autoFocus}
+            onKeyDown={handleInput}
+            type="text"
+            placeholder={placeholder}
+            className='py-3 px-4 block w-[300px] border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none ' />
     )
 }
 
