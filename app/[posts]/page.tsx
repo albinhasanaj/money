3a845818b6674e3e72b4c1f4c1b43107667f9e36
Oliversoder06 <lexrudolf06@gmail.com>
2024-07-31 15:03:20 +0200

    im stuck stepbro

--- app/[posts]/page.tsx
+++ app/[posts]/page.tsx
@@ -1,9 +1,10 @@
-import ContainersContainer from '@/components/ContainersContainer'
+"use client"
+import Container from '@/components/Container'
 import React from 'react'
 
 const Project = ({ params }: { params: { posts: string } }) => {
     const { posts } = params
-
+    const [showHelp, setShowHelp] = React.useState(true)
     let textToDisplay = ""
 
     if (posts == "home") {
@@ -13,10 +14,54 @@ const Project = ({ params }: { params: { posts: string } }) => {
     } else if (posts == "review") {
         textToDisplay = "Solved: 50%"
     }
+    const toggleHelp = () => {
+        setShowHelp(!showHelp)
+    }
+
     return (
         <div className=' bg-[#D1D1D1] w-full items-center flex flex-col pt-10'>
-            <p className='text-black text-[16px] w-[756px] text-start pb-2'>{textToDisplay}</p>
-            <ContainersContainer />
+            {showHelp ? (
+                <div>
+                    <p className='text-black text-[16px] w-[756px] text-start pb-2'>{textToDisplay}</p>
+                    <div className='flex flex-col gap-10'>
+                        <div className='flex items-center gap-3'>
+                            <Container
+                                header='Python Scanner'
+                                author='Albin'
+                                info='Projekt som skanna alla ip adresser å sen gör man så här som exempel jätte
+                                bra träning vi får lära oss om x å y å huir man implementerar cool sak som det här
+                                osv för det är jätte coolt du vet'
+                                isDone={true}
+                            />
+                            <a onClick={toggleHelp} className='text-black text-[16px] select-none cursor-pointer'>Stuck?</a>
+                        </div>
+                        <div className='flex items-center gap-3'>
+                            <Container
+                                header='Egen socket'
+                                author='Oliver'
+                                info='projekt där vi skapar egen socket med hjälp av ,,,, bra projekt för vi kan göra si å så 
+                                å du vet det är awesome jao'
+                                isDone={true}
+                            />
+                            <a onClick={toggleHelp} className='text-black text-[16px] select-none cursor-pointer'>Stuck?</a>
+                        </div>
+                        <div className='flex items-center gap-3'>
+                            <Container
+                                header='Recursion Algorithm'
+                                author='Rafey'
+                                info=' jksadpojfop ajsdpof jpadsojf paosdjf päsdjfp ojsadpf jdsapof japsdojf poasjf poasdjf psad
+                                f asdfojasdpof jasdpof jopadsjf opasdj
+                                f dasjf opåasjf dasopf jadspof 
+                                dasf dasjf oadsjf aos fjaosp fjasp ofjaspojf podasjf sa'
+                                isDone={true}
+                            />
+                            <a onClick={toggleHelp} className='text-black text-[16px] select-none cursor-pointer'>Stuck?</a>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <h1 onClick={toggleHelp}></h1>
+            )}
         </div>
     )
 }
