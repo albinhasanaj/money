cc6eb952fe51dbce68700ac1e68bef1b5c5acdc5
albinhasanaj <albinhasanaj06@hotmail.com>
2024-07-31 02:41:43 +0200

    navbar add again

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className='mt-2'>
         <ul className='flex gap-6 text-[20px] text-black'>
-            <li><Link href='/'>Home</Link></li>
+            <li><Link href='/home'>Home</Link></li>
             <li><Link href='/learn'>Learn</Link></li>
             <li><Link href='/paths'>Paths</Link></li>
             <li><Link href='/review'>Review</Link></li>
